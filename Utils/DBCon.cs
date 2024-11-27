65f3fb3c2f550835df6c805002b1d15fdfa40e42
viner <viner@DESKTOP-N3H1BFC>
2024-11-27 23:28:27 +0300

    измениние точки подключения

--- Utils/DBCon.cs
+++ Utils/DBCon.cs
@@ -5,7 +5,7 @@ namespace kursDB1.Utils
 {
     public static class DbConnection
     {
-        private static readonly string _connectionString = "your_connection_string_here";
+        private static readonly string _connectionString = "Host=localhost;Port=5432;Username=postgres;Password=2005;Database=db1";
         //  private static readonly string _connectionString = "Host=localhost;Port=5432;Username=postgres;Password=password123;Database=online_art_db";
 
         public static AppDbContext GetDbContext()
