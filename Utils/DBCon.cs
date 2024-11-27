e7e9b6ad28bf905d54ccd0609c17e9b4ed3dffc5
viner <viner@DESKTOP-N3H1BFC>
2024-11-27 23:21:33 +0300

    Добавление утилит

---
+++ Utils/DBCon.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace kursDB1.Utils
+{
+    public static class DbConnection
+    {
+        private static readonly string _connectionString = "your_connection_string_here";
+        //  private static readonly string _connectionString = "Host=localhost;Port=5432;Username=postgres;Password=password123;Database=online_art_db";
+
+        public static AppDbContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql(_connectionString); // Используем PostgreSQL, например
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Utils/PassHash.cs
@@ -0,0 +1,19 @@
+﻿using BCrypt.Net;
+
+namespace kursDB1.Utils
+{
+    public static class PasswordHasher
+    {
+        // Хэширует пароль
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        // Проверяет правильность пароля
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
