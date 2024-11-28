23f777950cccc024ba453733d17e5de2c1ba4031
viner <viner@DESKTOP-N3H1BFC>
2024-11-28 19:57:47 +0300

    Исправление ошибок в RegForm

--- Views/RegForm.Designer.cs
+++ Views/RegForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace kursDB1.Views
 {
-    partial class RegisterForm
+    partial class RegForm
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.TextBox txtUsername;
@@ -11,8 +11,15 @@
         private System.Windows.Forms.Label lblConfirmPassword;
         private System.Windows.Forms.Button btnRegister;
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-        // Инициализация компонентов
         private void InitializeComponent()
         {
             this.txtUsername = new System.Windows.Forms.TextBox();
@@ -24,78 +31,70 @@
             this.btnRegister = new System.Windows.Forms.Button();
 
             this.SuspendLayout();
-            // 
+
             // txtUsername
-            // 
             this.txtUsername.Location = new System.Drawing.Point(150, 30);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(200, 20);
             this.txtUsername.TabIndex = 0;
-            // 
+
             // txtPassword
-            // 
             this.txtPassword.Location = new System.Drawing.Point(150, 70);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(200, 20);
             this.txtPassword.TabIndex = 1;
-            // 
+
             // txtConfirmPassword
-            // 
             this.txtConfirmPassword.Location = new System.Drawing.Point(150, 110);
             this.txtConfirmPassword.Name = "txtConfirmPassword";
             this.txtConfirmPassword.PasswordChar = '*';
             this.txtConfirmPassword.Size = new System.Drawing.Size(200, 20);
             this.txtConfirmPassword.TabIndex = 2;
-            // 
+
             // lblUsername
-            // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Location = new System.Drawing.Point(30, 30);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(85, 13);
+            this.lblUsername.Size = new System.Drawing.Size(113, 13);
             this.lblUsername.TabIndex = 3;
             this.lblUsername.Text = "Имя пользователя:";
-            // 
+
             // lblPassword
-            // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Location = new System.Drawing.Point(30, 70);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(48, 13);
+            this.lblPassword.Size = new System.Drawing.Size(51, 13);
             this.lblPassword.TabIndex = 4;
             this.lblPassword.Text = "Пароль:";
-            // 
+
             // lblConfirmPassword
-            // 
             this.lblConfirmPassword.AutoSize = true;
             this.lblConfirmPassword.Location = new System.Drawing.Point(30, 110);
             this.lblConfirmPassword.Name = "lblConfirmPassword";
-            this.lblConfirmPassword.Size = new System.Drawing.Size(106, 13);
+            this.lblConfirmPassword.Size = new System.Drawing.Size(119, 13);
             this.lblConfirmPassword.TabIndex = 5;
             this.lblConfirmPassword.Text = "Подтвердите пароль:";
-            // 
+
             // btnRegister
-            // 
             this.btnRegister.Location = new System.Drawing.Point(150, 150);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(100, 30);
+            this.btnRegister.Size = new System.Drawing.Size(200, 30);
             this.btnRegister.TabIndex = 6;
             this.btnRegister.Text = "Зарегистрироваться";
             this.btnRegister.UseVisualStyleBackColor = true;
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
-            // 
-            // RegisterForm
-            // 
+
+            // RegForm
             this.ClientSize = new System.Drawing.Size(400, 200);
-            this.Controls.Add(this.btnRegister);
-            this.Controls.Add(this.lblConfirmPassword);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.txtConfirmPassword);
-            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUsername);
-            this.Name = "RegisterForm";
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.btnRegister);
+            this.Name = "RegForm";
             this.Text = "Регистрация пользователя";
             this.ResumeLayout(false);
             this.PerformLayout();
