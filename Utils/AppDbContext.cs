f6bab12fcfc51ecde6da8c0bb107f179b7870502
viner <viner@DESKTOP-N3H1BFC>
2024-11-28 14:38:14 +0300

    добавление AppDbContext

---
+++ Utils/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using kursDB1.Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace kursDB1.Utils
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=2005;Database=db1");
+        }
+    }
+}
--- Utils/DBCon.cs
+++ Utils/DBCon.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace kursDB1.Utils
 {
